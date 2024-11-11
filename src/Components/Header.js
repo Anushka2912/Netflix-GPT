afb402b1fcdb473809e814c625a9924e939652e2
Anushka Sharma <anushka626sata@gmail.com>
2024-11-11 00:22:53 -0500

    feat: add Netflix logo to Header component - Included Netflix logo image in the header for branding

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='headerContainer w-full max-w-[1920px] m-auto px-12 py-6'>
+        <div className='logoContainer'>
+            <img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" className='w-48 h-auto'></img>
+        </div>
+    </div>
   )
 }
 
