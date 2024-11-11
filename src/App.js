0c5d95b6e248b9bbd40b1f33c2bfbf75700a7051
Anushka Sharma <anushka626sata@gmail.com>
2024-11-10 23:57:06 -0500

    feat: add routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import Body from "./Components/Body";
+
  function App() {
   return (
-    <div className="App">
-      <h1 className='text-blue-700 font-bold text-[100px]'>Hello</h1>
-    </div>
+    <Body />
   );
 }
 
