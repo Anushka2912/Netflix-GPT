7ee9e819502fac82e81f1560a8723e4b9e293d1a
Anushka Sharma <anushka626sata@gmail.com>
2024-11-11 00:51:28 -0500

    feat: add background image with overlay to Sign In page - Implemented background image and overlay styling for Sign In page for improved design

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -3,8 +3,13 @@ import Header from './Header'
 
 const SignIn = () => {
   return (
-    <div>
-        <Header />
+    <div className='relative'>
+        <div className='bg-[url("https://assets.nflxext.com/ffe/siteui/vlv3/81d64f3c-9627-4741-8f74-422bf35f9f1d/web/CA-en-20241104-TRIFECTA-perspective_c9918c41-adde-428f-b9f1-3aa64bfa091f_medium.jpg")] h-screen bg-cover bg-no-repeat'>
+            <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50' />
+            <div className='relative z-10'>
+                <Header />
+            </div>
+        </div>
     </div>
   )
 }
