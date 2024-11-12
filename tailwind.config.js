8db2f66a8458725b8afc9015150e2daae1cdb046
Anushka Sharma <anushka626sata@gmail.com>
2024-11-11 20:15:05 -0500

    style: add custom colors to Tailwind CSS config - Updated tailwind.config.js with brand-specific color palette for consistent styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'red': '#e50914',
+      'hoverRed': '#c11119',
+      'black': '#000000',
+      'white': '#ffffff',
+      'gray': '#161616',
+      'lightGray': '#8c8c8c',
+      'grey': '#808080',
+      'blue': '#0071eb',
+    },
   },
   plugins: [],
 }
