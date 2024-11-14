6cd31aec6ffb00069f310fbd66bd057964181781
Anushka Sharma <anushka626sata@gmail.com>
2024-11-13 20:45:38 -0500

    chore: install and initialize Firebase, deploy project - Installed Firebase and set up configuration for authentication and database - Deployed project using Firebase hosting for live access

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBT0SsB4AlVc5ryZkqXLrFZB6ziei8QZDI",
+  authDomain: "netflix-gpt-9011f.firebaseapp.com",
+  projectId: "netflix-gpt-9011f",
+  storageBucket: "netflix-gpt-9011f.firebasestorage.app",
+  messagingSenderId: "129448136560",
+  appId: "1:129448136560:web:77bf74ce505c49fce249b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
