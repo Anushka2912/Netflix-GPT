0a0eafc9a869fa9b79303a3ec464ae69970ea8c7
Anushka Sharma <anushka626sata@gmail.com>
2024-11-19 22:51:11 -0500

    fix: redirect user based on authentication status

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,10 @@
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { removeUser } from "../utils/userSlice";
+import { addUser, removeUser } from "../utils/userSlice";
+import { useEffect } from "react";
+import { LOGO } from "../utils/constant ";
 
 const Header = () => {
 
@@ -12,13 +14,34 @@ const Header = () => {
   console.log({user});
 
   const handleSignOut = () => {
-    signOut(auth).then(() => {
-      dispatch(removeUser);
-      navigate("/");
-    }).catch((error) => {
-      //navigate("/error");
+    signOut(auth)
+    .then(() => {})
+    .catch((error) => {
+      navigate("/error");
     });
   }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const {uid, email, displayName, photoURL} = user;
+          dispatch(
+            addUser({
+              uid: uid, 
+              email: email, 
+              displayName: displayName, 
+              photoURL: photoURL
+            })
+          );
+          navigate("/browse");
+        } else {
+            dispatch(removeUser());
+            navigate("/");
+        }
+    });
+    //unsubscribe when component unmounts
+    return () => unsubscribe();
+  }, []);
 
   return (
     <div className='headerContainer w-full max-w-[1920px] m-auto px-12 py-6'>
@@ -27,7 +50,7 @@ const Header = () => {
             <img
               alt="logo" 
               className="w-48 h-auto" 
-              src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" 
+              src={LOGO}
             />
           </div>
           {user ?
