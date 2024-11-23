ffcb5e109dbc33c20f3386f32b148615e13e531b
Anushka Sharma <anushka626sata@gmail.com>
2024-11-22 23:37:05 -0500

    feat: Create MovieCard component to showcase individual movie details

---
+++ src/Components/MovieCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { IMG_CDN_URL } from '../utils/constant ';
+
+const MovieCard = ({posterPath}) => {
+    
+  return (
+    <div>
+        <img 
+            alt='Movie_Poster' 
+            src={IMG_CDN_URL + posterPath}
+            className='max-w-48'
+        />
+    </div>
+  );
+};
+
+export default MovieCard;
