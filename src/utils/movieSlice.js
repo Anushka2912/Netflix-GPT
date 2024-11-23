b1b797d2237900cb663661d973b3eaba9b4e2a5f
Anushka Sharma <anushka626sata@gmail.com>
2024-11-22 23:41:39 -0500

    feat: add actions for popular and upcoming movies in movie slice and update movie slice with reducers for new actions

--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -5,11 +5,19 @@ const movieSlice = createSlice (
         name: "movies",
         initialState: {
             nowPlayingMovies: null,
+            popularMovies: null,
+            upcomingMovies: null,
             trailerVideo: null,
         },
         reducers: {
             addNowPlayingMovies : (state, action) => {
                 state.nowPlayingMovies = action.payload;
+            },
+            addPopularMovies : (state, action) => {
+                state.popularMovies = action.payload;
+            },
+            addUpcomingMovies: (state, action) => {
+                state.upcomingMovies = action.payload;
             },
             addTrailerVideo : (state, action) => {
                 state.trailerVideo = action.payload;
@@ -18,6 +26,6 @@ const movieSlice = createSlice (
     }
 );
 
-export const {addNowPlayingMovies, addTrailerVideo} = movieSlice.actions;
+export const {addNowPlayingMovies, addPopularMovies, addUpcomingMovies, addTrailerVideo} = movieSlice.actions;
 
 export default movieSlice.reducer;
