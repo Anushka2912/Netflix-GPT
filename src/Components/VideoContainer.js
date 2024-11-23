d87e5e2e04b690656459073c19b0be5444af76fb
Anushka Sharma <anushka626sata@gmail.com>
2024-11-22 20:07:42 -0500

    style: enhance VideoContainer and components with Tailwind CSS

--- src/Components/VideoContainer.js
+++ src/Components/VideoContainer.js
@@ -14,7 +14,7 @@ const VideoContainer = () => {
     const {original_title, overview, id} = mainMovie;
 
     return (
-        <div className='bg-gray bg-opacity-30'>
+        <div className='relative w-screen -mt-[130px]'>
             <VideoBackground movieId={id}/>
             <VideoTitle title={original_title} overview={overview} />
         </div>
--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -13,15 +13,15 @@ const VideoTitle = ({title, overview}) => {
   // }, []);
 
   return (
-    <div className='py-20 px-12 w-1/4'>
-        <div>
-            <h1 className='text-5xl font-bold'>{title}</h1>
-            <p className='py-6 text-lg'>{overview}</p>
-        </div>
-        <div className='flex gap-3'>
-            <button className='bg-white rounded-[2px] text-black w-32 px-5 py-2'>Play</button>
-            <button className='bg-lightGray bg-opacity-60 rounded-[2px] text-white w-32 px-5 py-2'>More Info</button>
-        </div>
+    <div className='absolute bottom-0 left-0 w-screen aspect-video px-20 pt-[44%] bg-gradient-to-r from-black'>
+      <div>
+          <h1 className='text-white text-5xl font-bold'>{title}</h1>
+          <p className='text-white py-6 text-lg w-1/4'>{overview}</p>
+      </div>
+      <div className='flex gap-3'>
+          <button className='bg-white rounded-[4px] text-black font-bold w-32 px-5 py-2'>Play</button>
+          <button className='bg-grey bg-opacity-70 rounded-[4px] text-white font-bold w-32 px-5 py-2'>More Info</button>
+      </div>
     </div>
   );
 };
