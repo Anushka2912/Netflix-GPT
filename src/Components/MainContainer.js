5d646e6cdf8dbb2963494a16a3150496489611b1
Anushka Sharma <anushka626sata@gmail.com>
2024-11-22 23:37:57 -0500

    feat: integrate MovieList component into MainContainer

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import MovieList from './MovieList';
+import { useSelector } from 'react-redux';
 
 const MainContainer = () => {
+  const movies = useSelector((store => store.movies));
+
   return (
-    <div>MainContainer</div>
+    <div className='bg-black text-white'>
+        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+        <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
+        <MovieList title={"Popular"} movies={movies.popularMovies}/>
+        <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies}/>
+        <MovieList title={"Horror"} movies={movies.nowPlayingMovies}/>
+    </div>
   );
 };
 
