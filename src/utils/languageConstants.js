a273d6385e3d74a048d668ffffed376c20d42491
Anushka Sharma <anushka626sata@gmail.com>
2024-11-26 12:08:36 -0500

    feat: add language constants file for English and French text

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,12 @@
+const lang = {
+    en: {
+        searchBtnText: "Search",
+        gptSearchPlaceholder: "What would you like to watch today?",
+    },
+    fr: {
+        searchBtnText: "Recherche",
+        gptSearchPlaceholder: "Qu’aimeriez-vous regarder aujourd’hui ?",
+    },
+};
+
+export default lang;
