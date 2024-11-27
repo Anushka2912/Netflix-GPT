acc8490da4c0809c22dad1fd734e7e574939b5b1
Anushka Sharma <anushka626sata@gmail.com>
2024-11-27 12:27:57 -0500

    feat: make input placeholder and button dynamic using config slice - Update input placeholder and button text to change dynamically based on selected language - Utilize useSelector to fetch language settings from config slice

--- src/Components/GptSearchBar.js
+++ src/Components/GptSearchBar.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import lang from '../utils/languageConstants';
+import { useSelector } from 'react-redux';
 
 const GptSearchBar = () => {
+  const selectedLang = useSelector((store) =>  store.config.lang);
+
   return (
     <div>
         <form className='p-6 m-6 flex justify-center gap-3'>
             <input 
               type='text' 
               className='w-2/5 px-4 py-4 text-black bg-white border border-black rounded-[5px]' 
-              placeholder={lang.fr.gptSearchPlaceholder} 
+              placeholder={lang[selectedLang].gptSearchPlaceholder} 
             />
             <button className='w-[200px] py-2 px-4 bg-red border-red text-white rounded-[5px]'>
-              {lang.fr.searchBtnText}
+              {lang[selectedLang].searchBtnText}
             </button>
         </form>
     </div>
