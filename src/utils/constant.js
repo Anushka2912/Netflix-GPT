b1a37d77f0e7fb759836212b56f6a158e43f640e
Anushka Sharma <anushka626sata@gmail.com>
2024-11-29 15:22:04 -0500

    feat: remove api key from the constant file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -8,7 +8,7 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer '
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTMyNWE4M2RjNGEyN2JiMTUyYzJlNTdmNmFiZDlmYyIsIm5iZiI6MTczMjA3NTI3My42MDU2MjM1LCJzdWIiOiI2NzNkNWRmYzI0NzkxN2U5NWIyYWY1ZTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.4kytrVPfWElEIdBDYPN7qE6BkLyLjUAndhWeuDFSS2c'
   }
 };
 
@@ -16,5 +16,3 @@ export const SUPPORTED_LANGUAGES = [
   {identifier: "en", name: "English"}, 
   {identifier: "fr", name: "French"},
 ];
-
-export const OPENAI_KEY = null;
