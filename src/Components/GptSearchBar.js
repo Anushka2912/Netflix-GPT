5072a233b7be48d0986409a84a4e2c63d322d6f0
Anushka Sharma <anushka626sata@gmail.com>
2024-12-01 21:52:30 -0500

    feat: integrate TMDB API search with OpenAI results - Create searchMovieTMDB function to search for movies obtained from OpenAI results in TMDB Search movies API - Process OpenAI search results by splitting content into an array - Implement helper function searchMovieTMDB to call TMDB Search API for each movie - Use Promise.all to resolve all API calls and construct tmdbResults array with complete movie data

--- src/Components/GptSearchBar.js
+++ src/Components/GptSearchBar.js
@@ -2,23 +2,45 @@ import React, { useRef } from 'react';
 import lang from '../utils/languageConstants';
 import { useSelector } from 'react-redux';
 import openai from '../utils/openai';
+import { API_OPTIONS } from '../utils/constant';
 
 const GptSearchBar = () => {
   const selectedLang = useSelector((store) =>  store.config.lang);
-
   const searchText = useRef(null);
+
+  //Serach movie in TMDb after getting the search results
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query="+ movie +"&include_adult=false&language=en-US&page=1", API_OPTIONS
+    );
+    const json = await data.json();
+
+    return json.results;
+  };
 
   const handleGptSearchClick = async () => {
     console.log(searchText.current.value);
 
-    const gptQuery = "Act as a Movie Recommendatin System and suggest movie names for the query :" + searchText.current.value;
+    const gptQuery = "Act as a Movie Recommendatin System and suggest movie names for the query :" + searchText.current.value + "Seperate movie names with comma like this example: Movie1, Movie2";
 
     const gptResults = await openai.chat.completions.create({
       messages: [{ role: 'user', content: gptQuery }],
       model: 'gpt-3.5-turbo',
     });
 
-    console.log(gptResults.choices);
+    if(!gptResults.choices) {
+      // TODO: Write Error Handling
+    }
+    
+    console.log(gptResults.choices?.[0]?.message?.content);
+
+    const gptMovies = gptResults.choices?.[0]?.message?.content.split(", ");
+    
+    const promiseArray = gptMovies?.map((movie) => searchMovieTMDB(movie));
+
+    const tmdbResults = await Promise.all(promiseArray);
+
+    console.log({tmdbResults});
   };
 
   return (
